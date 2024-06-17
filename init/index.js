d7d614bba41e78722e79808c75b2bf0439e084ea
Sayeda Rahman Ananna <sayedarahmanananna@gmail.com>
2024-06-17 20:08:35 +0600

    Added Listing database

--- init/index.js
+++ init/index.js
@@ -1,18 +1,45 @@
+require('dotenv').config(); // Ensure to load environment variables
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wandelust";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wandelust";
 
-main().then(() => {
-    console.log("Connected to db");
-}) .catch ((err) => {
-    console.log(err); 
-})
+const dbUrl = process.env.ATLASDB_URL;
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
+    console.log("Connected to db");
 }
+main().catch((err) => {
+    console.error("Error connecting to db", err);
+}); 
+
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600,
+})
+
+store.on("error", () => {
+    console.log("ERROR IN MONGO SESSION STORE", err);
+});
+
+// main().then(() => {
+//     console.log("Connected to db");
+// }) .catch ((err) => {
+//     console.log(err); 
+// })
+
+// async function main() {
+//     await mongoose.connect(MONGO_URL);
+// }
 
 const initDB = async () => {
    await Listing.deleteMany({});
